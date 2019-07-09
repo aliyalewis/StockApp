1ccfa81f25fd82dabdfa6c73e5b11e8bdfc484d7
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-09 09:31:17 -0700

    faker, seed, comments views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,28 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user, optional: true
-  belongs_to :stock, optional: true
-
-  validate :there_can_be_only_one
-
-  private
-
-  def there_can_be_only_one
-    if stock_count + user_count != 1
-      errors.add(:base, "can only have one owner")
-    end
-  end
-
-  def stock_count
-    stock.present? ? 1 : 0
-  end
-
-  def user_count
-    user.present? ? 1 : 0
-  end
-
-  def owner
-    @owner ||= stock || library
-  end
-
+  belongs_to :user
+  belongs_to :stock
 
 end
--- db/migrate/20190708190340_create_stocks.rb
+++ db/migrate/20190708190340_create_stocks.rb
@@ -2,7 +2,6 @@ class CreateStocks < ActiveRecord::Migration[5.2]
   def change
     create_table :stocks do |t|
       t.string :symbol
-      t.string :company_name
 
       t.timestamps
     end
