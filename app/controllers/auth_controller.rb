0394281ce46617082f0901581036630e3c42c2b5
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-10 10:41:57 -0700

    login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,6 +3,16 @@ class AuthController < ApplicationController
     def new
     end
 
+    def verify
+        if User.find_by(username: params[:username])
+          @user = User.find_by(username: params[:username])
+          session[:user_id] = @user.id
+          redirect_to stocks_path
+        else
+            redirect_to new_user_path
+        end
+    end
+
     def destroy
         session.clear
     end
