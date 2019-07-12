275d4914f8d88fd90c699fd30e7d0cf1baeb62bc
github username <github email address>
2019-07-12 10:46:20 -0700

    ..

--- app/controllers/portfoliosstocks_controller.rb
+++ app/controllers/portfoliosstocks_controller.rb
@@ -5,33 +5,22 @@ class PortfoliosstocksController < ApplicationController
 
     def show
         @sps = PortfoliosStock.where(portfolio_id: params[:id])
-        @sps.each do |portfolio|
-           @stock = Stock.find(portfolio.stock_id)
-           @stock.symbol
-           portfolio.shares
-        end
-        fetch_stocks(@stock.symbol)
-       
-        
+        @stock = Stock.find_by(params[:symbol])
     end
 
     def new
         @ps = PortfoliosStock.new
-        
     end
 
     def create
         @portfolio = Portfolio.find_by(user_id: params[:user_id])
-       
-        @ps = PortfoliosStock.new(portfolio_id: @portfolio.id, stock_id: params[:portfoliosstock][:stock_id], shares: params[:shares])
+        @ps = PortfoliosStock.new(portfolio_id: @portfolio.id, stock_id: params[:portfoliosstock][:stock_id])
         if @ps.save
           redirect_to user_path(session[:user_id])
         else
           render :new
         end
     end
-
-
 
     def edit
         @user = User.find(params[:user_id])
