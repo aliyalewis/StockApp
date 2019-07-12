559e7938f1b6ae31204f8a59590f1d063ea56b6a
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-12 10:42:35 -0700

    ps added value to show page

--- app/controllers/portfoliosstocks_controller.rb
+++ app/controllers/portfoliosstocks_controller.rb
@@ -1,16 +1,17 @@
 class PortfoliosstocksController < ApplicationController
     def index
         @user = User.find(params[:user_id])
+        # @sps = PortfoliosStock.all
     end
 
     def show
         @sps = PortfoliosStock.where(portfolio_id: params[:id])
         @sps.each do |portfolio|
            @stock = Stock.find(portfolio.stock_id)
-           @stock.symbol
-           portfolio.shares
         end
-        fetch_stocks(@stock.symbol)
+        if @stock != nil
+            fetch_stocks(@stock.symbol)
+        end
        
         
     end
@@ -44,16 +45,5 @@ class PortfoliosstocksController < ApplicationController
         @portfolio = Portfolio.find_by(user_id: @user.id)
         @ps = PortfoliosStock.find_by(portfolio_id: @portfolio.id)
         @ps.update(:stock_id, :shares)
-        if @ps.update
-          redirect_to user_path(session[:user_id])
-        else
-          render :edit
-        end
-    end
-
-    def destroy
-      @stock = Stock.find_by(params[:symbol])
-      @stock.destroy
-      redirect_to user_portfoliosstock_path(session[:user_id])
     end
 end
